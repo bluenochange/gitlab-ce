20f0b5eb82218c1ca2f41c98510aebcf012362f5
Matija Čupić <matteeyah@gmail.com>
2018-09-19 19:46:59 +0200

    Apply background migration scheduling migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180916011959) do
+ActiveRecord::Schema.define(version: 20180916014356) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
