31e3e37792407dfef0cb63502488ae45af3032cd
Mark Lapierre <mlapierre@gitlab.com>
2018-09-21 17:35:45 -0400

    Retry login if page loads as guest

--- qa/qa/factory/resource/fork.rb
+++ qa/qa/factory/resource/fork.rb
@@ -13,9 +13,27 @@ module QA
 
         product(:user) { |factory| factory.user }
 
+        def visit_project_with_retry
+          start = Time.now
+
+          while Time.now - start < 20
+            push.project.visit!
+
+            return if Page::Menu::Main.act { has_personal_area?(wait: 1) }
+
+            Page::Menu::Main.act { sign_out }
+
+            Page::Main::Login.perform do |login|
+              login.sign_in_using_credentials(user)
+            end
+          end
+
+          raise "Failed to load project page and stay logged in"
+        end
+
         def fabricate!
           Capybara::Screenshot.screenshot_and_save_page
-          push.project.visit!
+          visit_project_with_retry
           Capybara::Screenshot.screenshot_and_save_page
           Page::Project::Show.act { fork_project }
 
